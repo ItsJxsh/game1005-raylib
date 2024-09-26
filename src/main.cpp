076df47ea9ea123162e1fc48e9caf2de740a8237
Joshua Hardwick <200560630@student.georgianc.on.ca>
2024-09-26 18:40:46 -0400

    Circle Drawing

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,22 @@ int main()
 {
     InitWindow(800, 800, "Game");
     SetTargetFPS(60);
+
+    float speed = 100.0f;
+    float position = 1.0f;
+
     while (!WindowShouldClose())
     {
+        float time = GetFrameTime();
+        position += speed * time;
+        Vector2 mousepos = GetMousePosition();
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World!", 10, 10, 20, GRAY);
+
+        DrawCircleV(mousepos, 25, RED);
+
         EndDrawing();
     }
 
